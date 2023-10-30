8d3e836b595848ef3ef708a9d12e18c6d8e7890e
deka2904 <114542800+deka2904@users.noreply.github.com>
2023-10-30 17:51:22 +0900

    1030 commit

--- src/main/java/com/SpringBoot_webEx/SpringBoot_webEx/chatbot/ServerController.java
+++ src/main/java/com/SpringBoot_webEx/SpringBoot_webEx/chatbot/ServerController.java
@@ -1,34 +1,34 @@
 package com.SpringBoot_webEx.SpringBoot_webEx.chatbot;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-@RestController
+@Controller
 public class ServerController {
-    @GetMapping("/chatbot/{query}")
-    public ResponseEntity<String> sendDataToServer(@PathVariable("query") String query) {
+    @GetMapping("/chatbot")
+    public String main(){
+        return "chat";
+    }
+
+    @PostMapping("/chatbot")
+    public String sendDataToServer(Model model, String query) {
         String data = String.format("{\"query\":\"%s\"}", query);
         RestTemplate restTemplate = new RestTemplate();
-        // Server A로 POST 요청 보냅니다.
         String serverAUrl = "http://127.0.0.1:5000/query/TEST";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-
         HttpEntity<String> chatRequest = new HttpEntity<>(data, headers);
-
         ResponseEntity<String> response = restTemplate.postForEntity(serverAUrl, chatRequest, String.class);
+        System.out.println(response.getBody());
 
-        System.out.println("Server A에서 받은 응답: " + response.getBody());
-        return response;
+        String q = response.getBody();
+        model.addAttribute("result_list", q);
+        return "chat";
     }
 }
